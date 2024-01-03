1a892207b9e3bdedfdbe6bbec17de406e70f3b6d
Josh Isaacson <65214680+Joshchilly@users.noreply.github.com>
2024-01-02 21:21:19 -0500

    Begin HTML layout and add JS logic
    - Begin writing the HTML for the different game screens
    - Add objects in JS script
    - Add assets folder to hold normalize.css

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # tic-tac-toe
 
-A Tic Tac Toe game which can be played in two modes: player versus player, or player versus computer.
+A Tic Tac Toe game which can be played in two modes: player versus player, player versus computer, or computer vs computer.
---
+++ script.js
@@ -0,0 +1,125 @@
+const gameboard = (function () {
+    const board = null;
+    const createNewBoard = () => board = new Array(9);
+    const deleteBoard = () => board = null;
+
+    function placeOnBoard(space, sign) {
+        board[space] = sign;
+    }
+
+    let fullBoard = false;
+    const checkIfFullBoard = () => fullBoard;
+    const setFullBoard = () => fullBoard = true;
+
+    function checkIfThreeInARow() {
+        // Define the collection of rows on the board 
+        const sets = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        // Check if any row in sets is contains only X or only O
+        sets.forEach(set => {
+            const [one, two, three] = set;
+            if (board[one] === board[two] && board[two] === board[three]) {
+                return true;
+            }
+        })
+
+        return false;
+    }
+
+    return { createNewBoard, deleteBoard, placeOnBoard, checkIfFullBoard, setFullBoard, checkIfThreeInARow };
+})();
+
+function createPlayer(name, sign) {
+    const name = name;
+    const getName = () => name;
+
+    const sign = sign;
+    const getSign = () => sign;
+
+    let playerTurn = false;
+    const getPlayerTurn = () => playerTurn;
+    const makePlayerTurn = () => playerTurn = true;
+    const endPlayerTurn = () => playerTurn = false;
+
+    return { getName, getSign, getPlayerTurn, makePlayerTurn, endPlayerTurn };
+}
+
+const displayController = (function () {
+    const screens = null;
+    function showHomeScreen() {
+
+    }
+
+    function showMenuScreen() {
+
+    }
+
+    function showGameScreen() {
+
+    }
+
+    function showRoundEndScreen() {
+
+    }
+
+    function showGameEndScreen() {
+
+    }
+
+    return { showHomeScreen, showMenuScreen, showGameScreen, showRoundEndScreen, showGameEndScreen };
+})();
+
+const events = (function () {
+    function setHomeScreenEventListeners() {
+
+    }
+
+    function setMenuScreenEventListeners() {
+
+    }
+
+    function setGameScreenEventListeners() {
+
+    }
+
+    function setRoundEndScreenEventListeners() {
+
+    }
+
+    function setGameEndScreenEventListeners() {
+
+    }
+
+    function setAllEventListeners() {
+        setHomeScreenEventListeners();
+        setMenuScreenEventListeners();
+        setGameScreenEventListeners();
+        setRoundEndScreenEventListeners();
+        setGameEndScreenEventListeners();
+    }
+
+    return { setAllEventListeners };
+})();
+
+const gameFlow = (function () {
+    function beginNewGame() {
+        events.setAllEventListeners();
+        displayController.showHomeScreen();
+        gameboard.createNewBoard();
+    }
+
+    function endGame() {
+        gameboard.deleteBoard();
+    }
+
+    return { beginNewGame, endGame };
+})();
